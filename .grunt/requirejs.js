d969e8873799491873a1fc10c516d6e6ac86edb9
Florian Friesdorf <flo@chaoflow.net>
2013-01-10 21:42:31 +0100

    whitespace

--- .grunt/requirejs.js
+++ .grunt/requirejs.js
@@ -1,23 +1,23 @@
 module.exports = {
-  options : {
-    baseUrl: 'src',
-    insertRequire: ["main"],
-    mainConfigFile: "src/main.js",
-    name: 'main',
-    optimize:'none'
-  },
-  build : {
     options : {
-      out : 'bundles/patterns-<%= meta.fingerprint %>.js'
+        baseUrl: 'src',
+        insertRequire: ["main"],
+        mainConfigFile: "src/main.js",
+        name: 'main',
+        optimize:'none'
+    },
+    build : {
+        options : {
+            out : 'bundles/patterns-<%= meta.fingerprint %>.js'
+        }
+    },
+    standalone : {
+        options : {
+            name : '../lib/almond',
+            include : 'main',
+            wrap : true,
+            out : 'bundles/patterns-standalone-<%= meta.fingerprint %>.js',
+            almond: true
+        }
     }
-  },
-  standalone : {
-    options : {
-      name : '../lib/almond',
-      include : 'main',
-      wrap : true,
-      out : 'bundles/patterns-standalone-<%= meta.fingerprint %>.js',
-      almond: true
-    }
-  }
 }
