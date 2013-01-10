33e7f69fae0e87408fdf9a8c23c9602f9a931526
Florian Friesdorf <flo@chaoflow.net>
2013-01-10 21:25:57 +0100

    missed .debug. in .grunt/requirejs

--- .grunt/requirejs.js
+++ .grunt/requirejs.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   build : {
     options : {
-      out : 'bundles/patterns.<%= meta.fingerprint %>.debug.js'
+      out : 'bundles/patterns.<%= meta.fingerprint %>.js'
     }
   },
   standalone : {
@@ -16,7 +16,7 @@ module.exports = {
       name : '../lib/almond',
       include : 'main',
       wrap : true,
-      out : 'bundles/patterns-standalone.<%= meta.fingerprint %>.debug.js',
+      out : 'bundles/patterns-standalone.<%= meta.fingerprint %>.js',
       almond: true
     }
   }
